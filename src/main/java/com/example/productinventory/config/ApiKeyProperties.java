f1e04610b30285cedeaca8e2553e3e3427ae3491
Jonathan Bohorquez <jonathan.bohorquez@tsicol.com>
2025-08-03 22:48:56 -0500

    Docker para containerizar los servicios.

--- src/main/java/com/example/productinventory/config/ApiKeyProperties.java
+++ src/main/java/com/example/productinventory/config/ApiKeyProperties.java
@@ -1,11 +1,9 @@
 package com.example.productinventory.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
-@Configuration
 @ConfigurationProperties(prefix = "api")
 public class ApiKeyProperties {
 
